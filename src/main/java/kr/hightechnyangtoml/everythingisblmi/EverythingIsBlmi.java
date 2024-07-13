f7e396aba11e60ba8c8e0ed2f37e5883a0613f98
카페모카 <ddooriddoori763@gmail.com>
2024-07-13 18:27:17 +0900

    init

---
+++ src/main/java/kr/hightechnyangtoml/everythingisblmi/EverythingIsBlmi.java
@@ -0,0 +1,17 @@
+package kr.hightechnyangtoml.everythingisblmi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EverythingIsBlmi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
